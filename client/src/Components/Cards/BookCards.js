34f13ce2c28e33fc013c7656440215b99c479dab
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-04 15:26:29 +0100

    API refresh

--- client/src/Components/Cards/BookCards.js
+++ client/src/Components/Cards/BookCards.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import './BookCards.css';
+import "./BookCards.css";
 
-import Form from '../Form/Form';
+import Form from "../Form/Form";
 
-export default function BookCards({books, handleDelete, handleUpdateBooks}) {
-
-//use to update movies being displayed   
-return (
+export default function BookCards({ books, handleDelete, handleUpdateBooks }) {
+  //use to update movies being displayed
+  return (
     <div>
-        {books.map(book => {
-            return (
-                <div key={book._id}>
+      {books?.map((book) => {
+        return (
+          <div key={book._id}>
+            <div>
+              <p onClick={() => handleDelete(book._id)}>X</p>
+              <h3>{book.name}</h3>
+              <img src={book.img_url} alt="book" />
+              <p>
+                {book.author}, {book.year}
+              </p>
+            </div>
 
-                    <div>
-                    <p onClick={() => handleDelete(book._id)}>X</p>
-                    <h3>{book.name}</h3>
-                    <img src={book.img_url} alt='book'/>
-                    <p>{book.author}, {book.year}</p>
-                    </div>
-
-                    <Form onSubmitFunc={handleUpdateBooks} book={book}/>
-                    </div>
-            )
-        })}
+            <Form onSubmitFunc={handleUpdateBooks} book={book} />
+          </div>
+        );
+      })}
     </div>
-)
+  );
 }
