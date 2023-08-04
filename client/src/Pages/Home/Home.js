92ef72c102c0e90aaa97d2fc9b2162d458c7cdad
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-04 09:01:42 +0100

    header add plus pages

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,23 +1,20 @@
-import React from 'react';
-
-import Hero from "../../Components/Hero/Hero"
-
+import React from "react";
 import { Helmet } from "react-helmet-async";
+import BookCards from "../../Components/Cards/BookCards";
 
 export default function Home() {
   return (
     <>
-    <Helmet>
+      {/* <Helmet>
         <title>Our Website</title>
-        <meta name="description" content="This is the home page" />
+        <meta name="description" content="This is the home page for Can of Books" />
         <link rel="cannonical" href='/' />
-    </Helmet>
-    <main>
-        <Hero />
-        <h2>This is the home page</h2>
-        <p>router checker</p>
-    </main>
+    </Helmet> */}
+      <main>
+        <h2>Home Page</h2>
+        {/* <p>router checker</p> */}
+        <BookCards />
+      </main>
     </>
-  )
+  );
 }
-
